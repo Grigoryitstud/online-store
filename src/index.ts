af3e330a764ef1fa35ddd5fcc71ad21cee22a67f
Grigiryitstud <initstudentitin@gmail.com>
2022-07-13 22:44:57 +0300

    feat: some filtration (Wed, Jul 13, 2022 10:44:57 PM)

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,104 @@
 import { products } from "./products";
-const products__main: HTMLElement = document.getElementById(
-  "products__main"
-) as HTMLElement;
-const category = document.getElementsByClassName(
-  "category"
-)  as HTMLCollectionOf<HTMLElement> ;
-const product = document.getElementsByClassName(
-  "product"
-)  as HTMLCollectionOf<HTMLElement> ;
-const dots = document.querySelector('.dots') as HTMLElement;
-const lines = document.querySelector('.lines') as HTMLElement;
+const products__main: HTMLElement = document.getElementById("products__main") as HTMLElement;
+const category = document.getElementsByClassName("category") as HTMLCollectionOf<HTMLElement>;
+const company = document.getElementById("company") as HTMLElement;
+const product = document.getElementsByClassName("product") as HTMLCollectionOf<HTMLElement>;
+const dots = document.querySelector(".dots") as HTMLElement;
+const lines = document.querySelector(".lines") as HTMLElement;
+
+interface slicer {
+  cost: number,
+  сategory: string,
+  available: string,
+  brand: string,
+  colors: string
+}
+let slicerProducts: slicer = {
+  cost: 3000,
+  сategory: "All",
+  available: "All",
+  brand: "All",
+  colors: "All"
+};
+// const slicerProducts: slicer;
 
 const setProducts = () => {
-  let imeges = products.map((prod) => `<div class="product active"><img class="product__img" src="${prod.img}" alt=""><div class="product__about"><h4>${prod.name}</h4><h4>${prod.cost}</h4><h4 class="inline">${prod.about}</h4><h4 class="inline">Available: ${prod.Available}</h4><h4 class="inline">SKU: ${prod.SKU}</h4><h4 class="inline">Brand: ${prod.Brand}</h4><h4 class="inline">Colors: ${prod.colors}</h4></div></div>`);
-
+  let imeges = products.map(
+    (prod) => {
+      if(slicerProducts.сategory !== "All") {
+        if(slicerProducts.available !== "All") {
+          if(slicerProducts.cost !== 3000) {
+            if(slicerProducts.brand !== "All") {
+              if(slicerProducts.colors !== "All") {
+                Array.from(prod.colors).forEach((el) => {
+                  if(el == slicerProducts.colors) {
+                        return `<div class="product active"><img class="product__img" src="${prod.img}" alt=""><div class="product__about"><h4>${prod.name}</h4><h4>${prod.cost}</h4><h4 class="inline">${prod.about}</h4><h4 class="inline">available: ${prod.available}</h4><h4 class="inline">SKU: ${prod.SKU}</h4><h4 class="inline">brand: ${prod.brand}</h4><h4 class="inline">Colors: ${prod.colors}</h4></div></div>`
+                      }
+                });
+              }
+              if(prod.cost == slicerProducts.cost) {
+                return `<div class="product active"><img class="product__img" src="${prod.img}" alt=""><div class="product__about"><h4>${prod.name}</h4><h4>${prod.cost}</h4><h4 class="inline">${prod.about}</h4><h4 class="inline">available: ${prod.available}</h4><h4 class="inline">SKU: ${prod.SKU}</h4><h4 class="inline">brand: ${prod.brand}</h4><h4 class="inline">Colors: ${prod.colors}</h4></div></div>`
+              }
+            }
+            if(prod.cost <= slicerProducts.cost) {
+              return `<div class="product active"><img class="product__img" src="${prod.img}" alt=""><div class="product__about"><h4>${prod.name}</h4><h4>${prod.cost}</h4><h4 class="inline">${prod.about}</h4><h4 class="inline">available: ${prod.available}</h4><h4 class="inline">SKU: ${prod.SKU}</h4><h4 class="inline">brand: ${prod.brand}</h4><h4 class="inline">Colors: ${prod.colors}</h4></div></div>`
+            }
+          }
+          if(prod.available == slicerProducts.сategory) {
+            return `<div class="product active"><img class="product__img" src="${prod.img}" alt=""><div class="product__about"><h4>${prod.name}</h4><h4>${prod.cost}</h4><h4 class="inline">${prod.about}</h4><h4 class="inline">available: ${prod.available}</h4><h4 class="inline">SKU: ${prod.SKU}</h4><h4 class="inline">brand: ${prod.brand}</h4><h4 class="inline">Colors: ${prod.colors}</h4></div></div>`
+          }
+        }
+        if(prod.сategory == slicerProducts.сategory) {
+          return `<div class="product active"><img class="product__img" src="${prod.img}" alt=""><div class="product__about"><h4>${prod.name}</h4><h4>${prod.cost}</h4><h4 class="inline">${prod.about}</h4><h4 class="inline">available: ${prod.available}</h4><h4 class="inline">SKU: ${prod.SKU}</h4><h4 class="inline">brand: ${prod.brand}</h4><h4 class="inline">Colors: ${prod.colors}</h4></div></div>`
+        }
+      } else return `<div class="product active"><img class="product__img" src="${prod.img}" alt=""><div class="product__about"><h4>${prod.name}</h4><h4>${prod.cost}</h4><h4 class="inline">${prod.about}</h4><h4 class="inline">available: ${prod.available}</h4><h4 class="inline">SKU: ${prod.SKU}</h4><h4 class="inline">brand: ${prod.brand}</h4><h4 class="inline">Colors: ${prod.colors}</h4></div></div>`
+    }
+     
+  );
   products__main.innerHTML = imeges.join("");
 };
 setProducts();
 const changeDotsClass = () => {
-  dots.classList.add('active');
-  lines.classList.remove('active');
-  products__main.classList.remove('active');
+  dots.classList.add("active");
+  lines.classList.remove("active");
+  products__main.classList.remove("active");
 };
 const changeLinesClass = () => {
-  lines.classList.add('active');
-  dots.classList.remove('active');
-  products__main.classList.add('active');
+  lines.classList.add("active");
+  dots.classList.remove("active");
+  products__main.classList.add("active");
 };
 const changeCategory = (a: string) => {
- console.log(a);
-}
-Array.from(category).forEach(box => {
-  box.addEventListener('click', function(){ changeCategory(box.innerText); })
+  slicerProducts.сategory = a;
+  setProducts();
+  console.log(a);
+};
+const changeCompany = (a: string) => {
+  slicerProducts.brand = a;
+  setProducts();
+  console.log(a);
+};
+Array.from(category).forEach((box) => {
+  box.addEventListener("click", function () {
+    changeCategory(box.innerText);
+  });
   console.log(box.innerText);
 });
 
-  // for (let i = 0; i < category.length; i++) {
-  //   category[i].addEventListener('click', changeCategory(category[i].innerHTML: any));
-  // }
+company.onchange = () => {
+  console.log(company.value);
+};
 
-dots.addEventListener('click', changeDotsClass);
-lines.addEventListener('click', changeLinesClass);
+// Array.from(company).forEach((box) => {
+//   box.addEventListener("click", function () {
+//     changeCompany(box.innerText);
+//   });
+//   console.log(box.innerText);
+// });
+
+// for (let i = 0; i < category.length; i++) {
+//   category[i].addEventListener('click', changeCategory(category[i].innerHTML: any));
+// }
+
+dots.addEventListener("click", changeDotsClass);
+lines.addEventListener("click", changeLinesClass);
--- src/products.ts
+++ src/products.ts
@@ -1,18 +1,14 @@
-interface Colors {
-    [index: number]: string;
-}
-
 interface Product{
     id: number;
     img: string;
     name: string;
-    cost: string;
+    cost: number;
     сategory: string,
     about: string;
-    Available: string;
+    available: string;
     SKU: string;
-    Brand: string;
-    colors?: Colors;
+    brand: string;
+    colors: string[];
 }
 
 
@@ -21,203 +17,204 @@ export const products: Product[]= [
     id: 1,
     img: "../img/1.jpg",
     name: "Modern Poster",
-    cost: "$30.99",
+    cost: 30.99,
     сategory: "Living Room",
     about: "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
-    Available: "In Stock",
+    available: "In Stock",
     SKU: "RecQ0fMd8T0Vk211E",
-    Brand: "Liddy",
+    brand: "Liddy",
     colors: ["black"]
 },
 {
     id: 2,
     name: "Bar Stool",
     img: "../img/2.jpg",
-    cost: "$40.99",
+    cost: 40.99,
     сategory: "Kitchen",
     about: "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
-    Available: "In Stock",
+    available: "In Stock",
     SKU: "RecoM2MyHJGHLVi5l",
-    Brand: "Liddy",
+    brand: "Liddy",
     colors: ["black", "green"]
 },
 {
     id: 3,
     name: "Armchair",
     img: "../img/3.jpg",
-    cost: "$125.99",
+    cost:125.99,
     сategory: "Office",
     about: "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
-    Available: "In Stock",
+    available: "In Stock",
     SKU: "Recd1jIVIEChmiwhe",
-    Brand: "Marcos",
+    brand: "Marcos",
     colors: ["black", "green", "blue"]
 },
 {
     id: 4,
     name: "Suede Armchair",
     img: "../img/4.jpeg",
-    cost: "$159.99",
+    cost:159.99,
     сategory: "Bedroom",
     about: "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
-    Available: "In Stock",
+    available: "In Stock",
     SKU: "RecroK1VD8qVdMP5H",
-    Brand: "Caressa",
+    brand: "Caressa",
     colors: ["gold", "green"]
 },
 {
     id: 5,
     name: "Leather Chair",
     img: "../img/5.jpg",
-    cost: "$200.99",
+    cost:200.99,
     сategory: "Living Room",
     about: "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
-    Available: "In Stock",
+    available: "In Stock",
     SKU: "RecoW8ecgjtKx2Sj2",
-    Brand: "Caressa",
+    brand: "Caressa",
     colors: ["gold", "green", "red"]
 },
 {
     id: 6,
     name: "Emperor Bed",
     img: "../img/6.jpg",
-    cost: "$239.99",
+    cost:239.99,
     сategory: "Bedroom",
     about: "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
-    Available: "Out Of Stock",
+    available: "Out Of Stock",
     SKU: "Rec1Ntk7siEEW9ha1",
-    Brand: "Ikea"
+    brand: "Ikea",
+    colors: ["no"]
 },
 {
     id: 7,
     name: "Accent Chair",
     img: "../img/7.jpeg",
-    cost: "$259.99",
+    cost:259.99,
     сategory: "Living Room",
     about: "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
-    Available: "In Stock",
+    available: "In Stock",
     SKU: "RecZkNf2kwmdBcqd0",
-    Brand: "Marcos",
+    brand: "Marcos",
     colors: ["blue", "green", "red"]
 },
 {
     id: 8,
     name: "Dining Table",
     img: "../img/8.jpg",
-    cost: "$429.99",
+    cost: 429.99,
     сategory: "Dining",
     about: "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
-    Available: "In Stock",
+    available: "In Stock",
     SKU: "RecotY5Nh00DQFdkm",
-    Brand: "Ikea",
+    brand: "Ikea",
     colors: ["blue", "green", "red"]
 },
 {
     id: 9,
     name: "Entertainment Center",
     img: "../img/9.jpg",
-    cost: "$599.99",
+    cost: 599.99,
     сategory: "Living Room",
     about: "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
-    Available: "In Stock",
+    available: "In Stock",
     SKU: "RecNZ0koOqEmilmoz",
-    Brand: "Caressa",
+    brand: "Caressa",
     colors: ["black", "red"]
 },
 {
     id: 10,
     name: "Utopia Sofa",
     img: "../img/10.jpg",
-    cost: "$799.99",
+    cost: 799.99,
     сategory: "Living Room",
     about: "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
-    Available: "In Stock",
+    available: "In Stock",
     SKU: "Rec7JInsuCEHgmaGe",
-    Brand: "Liddy",
+    brand: "Liddy",
     colors: ["red", "green"]
 },
 {
     id: 11,
     name: "Leather Sofa",
     img: "../img/11.jpg",
-    cost: "$999.99",
+    cost: 999.99,
     сategory: "Living Room",
     about: "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
-    Available: "In Stock",
+    available: "In Stock",
     SKU: "RecEOA6qtDag1hRbU",
-    Brand: "Caressa",
+    brand: "Caressa",
     colors: ["blue", "green", "red"]
 },
 {
     id: 12,
     name: "Vase Table",
     img: "../img/12.jpeg",
-    cost: "$1,209.99",
+    cost: 1.209,
     сategory: "Bedroom",
     about: "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
-    Available: "In Stock",
+    available: "In Stock",
     SKU: "Rec3jeKnhInKHJuz2",
-    Brand: "Marcos",
+    brand: "Marcos",
     colors: ["red", "green"]
 },
 {
     id: 13,
     name: "Sofa Set",
     img: "../img/13.jpeg",
-    cost: "$1,299.99",
+    cost: 1.299,
     сategory: "Office",
     about: "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
-    Available: "In Stock",
+    available: "In Stock",
     SKU: "Recs5BSVU3qQrOj4E",
-    Brand: "Marcos",
+    brand: "Marcos",
     colors: ["gold", "green"]
 },
 {
     id: 14,
     name: "Wooden Desk",
     img: "../img/14.jpeg",
-    cost: "$1,509.99",
+    cost: 1.509,
     сategory: "Kids",
     about: "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
-    Available: "In Stock",
+    available: "In Stock",
     SKU: "RecJIjREF3dlFi3sR",
-    Brand: "Ikea",
+    brand: "Ikea",
     colors: ["black", "green", "blue"]
 },
 {
     id: 15,
     name: "Wooden Table",
     img: "../img/15.jpg",
-    cost: "$2,349.99",
+    cost: 2.349,
     сategory: "Dining",
     about: "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
-    Available: "In Stock",
+    available: "In Stock",
     SKU: "RectfNsySwAJeWDN2",
-    Brand: "Caressa",
+    brand: "Caressa",
     colors: ["gold", "red", "blue"]
 },
 {
     id: 16,
     name: "Wooden Bed",
     img: "../img/16.jpg",
-    cost: "$2,500.99",
+    cost: 2.500,
     сategory: "Bedroom",
     about: "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
-    Available: "In Stock",
+    available: "In Stock",
     SKU: "Recv2ohxljlK2FZO7",
-    Brand: "Ikea",
+    brand: "Ikea",
     colors: ["black", "gold"]
 },
 {
     id: 17,
     name: "Albany Table",
     img: "../img/17.jpeg",
-    cost: "$3,099.99",
+    cost: 3.099,
     сategory: "Dining",
     about: "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
-    Available: "In Stock",
+    available: "In Stock",
     SKU: "Rec5NBwZ5zCD9nfF0",
-    Brand: "Liddy",
+    brand: "Liddy",
     colors: ["gold", "blue"]
 },
 ];
